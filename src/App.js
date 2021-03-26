083eb27e926b11e1ef2efd6d08681445f3fd016d
Natasha <natashacolusso@gmail.com>
2021-03-26 14:37:05 -0700

    Feature: Users displaying on screen grid format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
-        <h1>Hello</h1>
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
