4fedf9951ff830fb5dae36c78bbb649b077db63a
Natasha <natashacolusso@gmail.com>
2021-03-29 16:56:21 -0700

    Refactor: Separate page created for home and updated route in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
+import Home from "./components/pages/Home";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 
@@ -20,16 +19,7 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <>
-                      <Search />
-                      <Users />
-                    </>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
               </Switch>
